f2f3cd922305b3aaf0b7056c375dfe9a3303097a
David Dymko <dymkod@gmail.com>
2020-02-12 12:54:56 -0500

    setup main

--- main.go
+++ main.go
@@ -1,9 +1,28 @@
 package main
 
 import (
+	"fmt"
+	"math/rand"
+	"os"
+	"time"
 
+	"k8s.io/component-base/logs"
+	"k8s.io/kubernetes/cmd/cloud-controller-manager/app"
+
+	_ "k8s.io/component-base/metrics/prometheus/clientgo"
+	_ "k8s.io/component-base/metrics/prometheus/version" // for version metrics registration
 )
 
 func main() {
+	rand.Seed(time.Now().UnixNano())
 
+	command := app.NewCloudControllerManagerCommand()
+
+	logs.InitLogs()
+	defer logs.FlushLogs()
+
+	if err := command.Execute(); err != nil {
+		fmt.Fprintf(os.Stderr, "error: %v\n", err)
+		os.Exit(1)
+	}
 }
