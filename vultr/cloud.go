2c97ee2455142806756522d1e97edfc94ede39dd
David Dymko <dymkod@gmail.com>
2020-02-18 12:54:10 -0500

    setup cloud + zones

--- vultr/cloud.go
+++ vultr/cloud.go
@@ -1,6 +1,10 @@
 package vultr
 
 import (
+	"fmt"
+	"io"
+	"os"
+
 	"github.com/vultr/govultr"
 	cloudprovider "k8s.io/cloud-provider"
 	"k8s.io/klog"
@@ -8,7 +12,8 @@ import (
 
 const (
 	providerName   = "vultr"
-	accessTokenENV = "VULTR_API_KEY"
+	accessTokenEnv = "VULTR_API_KEY"
+	regionEnv      = "VULTR_REGION"
 )
 
 type cloud struct {
@@ -16,6 +21,34 @@ type cloud struct {
 	instances     cloudprovider.Instances
 	zones         cloudprovider.Zones
 	loadbalancers cloudprovider.LoadBalancer
+}
+
+func init() {
+	cloudprovider.RegisterCloudProvider(providerName, func(config io.Reader) (i cloudprovider.Interface, err error) {
+		return newCloud()
+	})
+}
+
+func newCloud() (cloudprovider.Interface, error) {
+	apiToken := os.Getenv(accessTokenEnv)
+	if apiToken == "" {
+		return nil, fmt.Errorf("%s must be set in the environment (use a k8s secret)", accessTokenEnv)
+	}
+
+	region := os.Getenv(regionEnv)
+	if region == "" {
+		return nil, fmt.Errorf("%s must be set in the environment (use a k8s secret)", regionEnv)
+	}
+
+	vultr := govultr.NewClient(nil, apiToken)
+	vultr.SetUserAgent(fmt.Sprintf("vultr-cloud-controller-manager %s", vultr.UserAgent))
+
+	return &cloud{
+		client: vultr,
+		//instances:     n,
+		zones: newZones(vultr, region),
+		//loadbalancers: nil,
+	}, nil
 }
 
 func (c *cloud) Initialize(clientBuilder cloudprovider.ControllerClientBuilder, stop <-chan struct{}) {
@@ -30,12 +63,13 @@ func (c *cloud) Instances() (cloudprovider.Instances, bool) {
 }
 
 func (c *cloud) Zones() (cloudprovider.Zones, bool) {
+	klog.V(5).Info("called Zones")
 	return c.zones, true
 }
 
 func (c *cloud) Clusters() (cloudprovider.Clusters, bool) {
 	klog.V(5).Info("called Clusters")
-	panic("implement me")
+	return nil, false
 }
 
 func (c *cloud) Routes() (cloudprovider.Routes, bool) {
@@ -50,5 +84,5 @@ func (c *cloud) ProviderName() string {
 
 func (c *cloud) HasClusterID() bool {
 	klog.V(5).Info("called HasClusterID")
-	panic("implement me")
+	return false
 }
--- vultr/zones.go
+++ vultr/zones.go
@@ -2,22 +2,43 @@ package vultr
 
 import (
 	"context"
+
+	"github.com/vultr/govultr"
 	"k8s.io/apimachinery/pkg/types"
 	"k8s.io/cloud-provider"
 )
 
-type zones struct{
-
+type zones struct {
+	client *govultr.Client
+	region string
 }
 
-func (z zones) GetZone(ctx context.Context) (cloudprovider.Zone, error) {
-	panic("implement me")
+func newZones(client *govultr.Client, zone string) cloudprovider.Zones {
+	return zones{client, zone}
+}
+
+func (z zones) GetZone(_ context.Context) (cloudprovider.Zone, error) {
+	return cloudprovider.Zone{Region: z.region}, nil
 }
 
 func (z zones) GetZoneByProviderID(ctx context.Context, providerID string) (cloudprovider.Zone, error) {
-	panic("implement me")
+	id, err := vultrIDFromProviderID(providerID)
+	if err != nil {
+		return cloudprovider.Zone{}, nil
+	}
+	vultr, err := vultrByID(ctx, z.client, id)
+	if err != nil {
+		return cloudprovider.Zone{}, err
+	}
+
+	return cloudprovider.Zone{Region: vultr.RegionID,}, nil
 }
 
 func (z zones) GetZoneByNodeName(ctx context.Context, nodeName types.NodeName) (cloudprovider.Zone, error) {
-	panic("implement me")
+	vultr, err := vultrByName(ctx, z.client, nodeName)
+	if err != nil {
+		return cloudprovider.Zone{}, nil
+	}
+
+	return cloudprovider.Zone{Region: vultr.RegionID}, nil
 }
