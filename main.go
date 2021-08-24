4ee85b289788f9ce269d37106c82897e29b34c87
David Dymko <dymkod@gmail.com>
2021-08-23 20:21:43 -0400

    updates

--- main.go
+++ main.go
@@ -1,12 +1,55 @@
 package main
 
 import (
-	_ "k8s.io/cloud-provider"
+	"fmt"
+	"math/rand"
+	"os"
+	"time"
 
-	_ "github.com/vultr/vultr-cloud-controller-manager/vultr"
-	_ "k8s.io/component-base/metrics/prometheus/clientgo"
-	_ "k8s.io/component-base/metrics/prometheus/version" // for version metrics registration
+	"github.com/vultr/vultr-cloud-controller-manager/vultr"
+	"k8s.io/apimachinery/pkg/util/wait"
+	cloudprovider "k8s.io/cloud-provider"
+	"k8s.io/cloud-provider/app"
+	"k8s.io/cloud-provider/app/config"
+	"k8s.io/cloud-provider/options"
+	"k8s.io/component-base/cli/flag"
+	"k8s.io/component-base/logs"
+	_ "k8s.io/component-base/metrics/prometheus/clientgo" // load all the prometheus client-go plugins
+	_ "k8s.io/component-base/metrics/prometheus/version"  // for version metric registration
+	"k8s.io/klog/v2"
 )
 
 func main() {
+	rand.Seed(time.Now().UnixNano())
+
+	ccmOptions, err := options.NewCloudControllerManagerOptions()
+	if err != nil {
+		klog.Fatalf("unable to initialize command options: %v", err)
+	}
+	ccmOptions.KubeCloudShared.CloudProvider.Name = vultr.ProviderName
+	//todo check the skip auth check
+
+	command := app.NewCloudControllerManagerCommand(ccmOptions, cloudInitializer, app.DefaultInitFuncConstructors, flag.NamedFlagSets{}, wait.NeverStop)
+
+	logs.InitLogs()
+	defer logs.FlushLogs()
+
+	if err := command.Execute(); err != nil {
+		fmt.Fprintf(os.Stderr, "error: %v\n", err)
+		os.Exit(1)
+	}
+}
+
+func cloudInitializer(config *config.CompletedConfig) cloudprovider.Interface {
+	cloudConfig := config.ComponentConfig.KubeCloudShared.CloudProvider
+	// initialize cloud provider with the cloud provider name and config file provided
+	cloud, err := cloudprovider.InitCloudProvider(cloudConfig.Name, cloudConfig.CloudConfigFile)
+	if err != nil {
+		klog.Fatalf("Cloud provider could not be initialized: %v", err)
+	}
+	if cloud == nil {
+		klog.Fatalf("Cloud provider is nil")
+	}
+
+	return cloud
 }
--- vultr/cloud.go
+++ vultr/cloud.go
@@ -15,9 +15,8 @@ import (
 )
 
 const (
-	providerName   = "vultr"
+	ProviderName   = "vultr"
 	accessTokenEnv = "VULTR_API_KEY"
-	regionEnv      = "VULTR_REGION"
 	userAgent      = "CCM_USER_AGENT"
 )
 
@@ -29,7 +28,7 @@ type cloud struct {
 }
 
 func init() {
-	cloudprovider.RegisterCloudProvider(providerName, func(config io.Reader) (i cloudprovider.Interface, err error) {
+	cloudprovider.RegisterCloudProvider(ProviderName, func(config io.Reader) (i cloudprovider.Interface, err error) {
 		return newCloud()
 	})
 }
@@ -101,7 +100,7 @@ func (c *cloud) Routes() (cloudprovider.Routes, bool) {
 
 func (c *cloud) ProviderName() string {
 	klog.V(5).Info("called ProviderName")
-	return providerName
+	return ProviderName
 }
 
 func (c *cloud) HasClusterID() bool {
