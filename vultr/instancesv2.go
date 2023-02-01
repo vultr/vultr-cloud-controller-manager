fe8d24a68e28ae3190a5da18c6828820bae13a0f
ErwinsExpertise <eevans@vultr.com>
2023-02-01 16:35:55 -0500

    instanceexists update

--- vultr/instancesv2.go
+++ vultr/instancesv2.go
@@ -4,6 +4,9 @@ package vultr
 import (
 	"context"
 	"fmt"
+	"log"
+	"reflect"
+	"strings"
 
 	"github.com/vultr/govultr/v2"
 	v1 "k8s.io/api/core/v1"
@@ -29,12 +32,17 @@ func newInstancesV2(client *govultr.Client) cloudprovider.InstancesV2 {
 
 // InstanceExists return bool whether or not the instance exists
 func (i *instancesv2) InstanceExists(ctx context.Context, node *v1.Node) (bool, error) {
-	newNode, err := vultrByName(ctx, i.client, types.NodeName(node.Name))
+	newNode, err := i.getVultrInstance(ctx, node)
 	if err != nil {
-		return false, nil
+		log.Printf("instance(%s) exists check failed: %e", node.Spec.ProviderID, err) //nolint
+		if strings.Contains(err.Error(), "invalid instance ID") {
+			return false, nil
+		}
+		return false, err
 	}
 
 	if newNode.Status == ACTIVE || newNode.Status == PENDING || newNode.Status == RESIZING {
+		log.Printf("instance(%s) status is: %s", newNode.Label, newNode.Status) //nolint
 		return true, nil
 	}
 
@@ -43,8 +51,9 @@ func (i *instancesv2) InstanceExists(ctx context.Context, node *v1.Node) (bool, 
 
 // InstanceShutdown returns bool whether or not the instance is running or powered off
 func (i *instancesv2) InstanceShutdown(ctx context.Context, node *v1.Node) (bool, error) {
-	newNode, err := vultrByName(ctx, i.client, types.NodeName(node.Name))
+	newNode, err := i.getVultrInstance(ctx, node)
 	if err != nil {
+		log.Printf("instance(%s) shutdown check failed: %e", node.Spec.ProviderID, err) //nolint
 		return false, err
 	}
 
@@ -57,8 +66,9 @@ func (i *instancesv2) InstanceShutdown(ctx context.Context, node *v1.Node) (bool
 
 // InstanceMetadata returns a struct of type InstanceMetadata containing the node information
 func (i *instancesv2) InstanceMetadata(ctx context.Context, node *v1.Node) (*cloudprovider.InstanceMetadata, error) {
-	newNode, err := vultrByName(ctx, i.client, types.NodeName(node.Name))
+	newNode, err := i.getVultrInstance(ctx, node)
 	if err != nil {
+		log.Printf("instance(%s) metadata check failed: %e", node.Spec.ProviderID, err) //nolint
 		return nil, err
 	}
 
@@ -67,17 +77,25 @@ func (i *instancesv2) InstanceMetadata(ctx context.Context, node *v1.Node) (*clo
 		return nil, err
 	}
 
-	return &cloudprovider.InstanceMetadata{
+	vultrNode := cloudprovider.InstanceMetadata{
 		InstanceType:  newNode.Plan,
 		ProviderID:    fmt.Sprintf("vultr://%s", newNode.ID),
 		Region:        newNode.Region,
 		NodeAddresses: nodeAddress,
-	}, nil
+	}
+
+	log.Printf("returned node metadata: %v", vultrNode) //nolint
+
+	return &vultrNode, nil
 }
 
 // nodeAddresses gathers public/private IP addresses and returns a []v1.NodeAddress .
 func (i *instancesv2) nodeAddresses(instance *govultr.Instance) ([]v1.NodeAddress, error) {
 	var addresses []v1.NodeAddress
+
+	if reflect.DeepEqual(instance, *&govultr.Instance{}) {
+		return nil, fmt.Errorf("instance is empty %v", instance)
+	}
 
 	addresses = append(addresses, v1.NodeAddress{
 		Type:    v1.NodeHostName,
@@ -95,4 +113,44 @@ func (i *instancesv2) nodeAddresses(instance *govultr.Instance) ([]v1.NodeAddres
 	)
 
 	return addresses, nil
+}
+
+// getVultrInstance attempts to obtain Vultr Instance from Vultr API
+func (i *instancesv2) getVultrInstance(ctx context.Context, node *v1.Node) (*govultr.Instance, error) {
+	skipID := false
+
+	if node.Spec.ProviderID == "" {
+		skipID = true
+	}
+
+	if node.Name == "" {
+		return nil, fmt.Errorf("node name cannot be empty")
+	}
+
+	if !skipID {
+		id, err := vultrIDFromProviderID(node.Spec.ProviderID)
+		if err != nil {
+			log.Printf("instance(%s) provider split failed: %e", node.Spec.ProviderID, err) //nolint
+			return nil, err
+		}
+
+		newNode, err := vultrByID(ctx, i.client, id)
+		if err != nil {
+			log.Printf("instance(%s) by ID failed: %e", node.Spec.ProviderID, err) //nolint
+			return nil, err
+		}
+
+		return newNode, nil
+
+	} else {
+		newNode, err := vultrByName(ctx, i.client, types.NodeName(node.Name))
+		if err != nil {
+			log.Printf("instance(%s) by name failed: %e", node.Name, err) //nolint
+			return nil, err
+		}
+
+		return newNode, nil
+
+	}
+
 }
